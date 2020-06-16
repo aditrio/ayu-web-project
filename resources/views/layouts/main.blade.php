f6a182437004248320e7c19b32bc5af1d936b1c9
aditrio <30noadi@gmail.com>
2020-06-17 03:22:28 +0700

    3rd

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,8 +3,8 @@
 
 <head>
   <meta charset="utf-8" />
-  <link rel="apple-touch-icon" sizes="76x76" href="{{ asset ('assets/img/apple-icon.png')}}">
-  <link rel="icon" type="image/png" href="{{ asset ('assets/img/favicon.png') }}">
+  <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="./assets/img/favicon.png">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
   <title>
     SiSehat
@@ -14,9 +14,9 @@
   <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
   <!-- CSS Files -->
-  <link href="{{ asset ('assets/css/material-dashboard.css?v=2.1.0')}}" rel="stylesheet" />
+  <link href="./assets/css/material-dashboard.css?v=2.1.0" rel="stylesheet" />
   <!-- CSS Just for demo purpose, don't include it in your project -->
-  <link href="{{ asset ('assets/demo/demo.css') }}" rel="stylesheet" />
+  <link href="./assets/demo/demo.css" rel="stylesheet" />
 </head>
 
 <body class="dark-edition">
@@ -129,23 +129,23 @@
     </div>
   </div>
   <!--   Core JS Files   -->
-  <script src="{{ asset ('assets/js/core/jquery.min.js') }}"></script>
-  <script src="{{ asset ('assets/js/core/popper.min.js') }}"></script>
-  <script src="{{ asset ('assets/js/core/bootstrap-material-design.min.js') }}"></script>
+  <script src="./assets/js/core/jquery.min.js"></script>
+  <script src="./assets/js/core/popper.min.js"></script>
+  <script src="./assets/js/core/bootstrap-material-design.min.js"></script>
   <script src="https://unpkg.com/default-passive-events"></script>
-  <script src="{{ asset ('assets/js/plugins/perfect-scrollbar.jquery.min.js') }}"></script>
+  <script src="./assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
   <!-- Place this tag in your head or just before your close body tag. -->
   <script async defer src="https://buttons.github.io/buttons.js"></script>
   <!--  Google Maps Plugin    -->
   <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
   <!-- Chartist JS -->
-  <script src="{{ asset ('assets/js/plugins/chartist.min.js') }}"></script>
+  <script src="./assets/js/plugins/chartist.min.js"></script>
   <!--  Notifications Plugin    -->
-  <script src="{{ asset ('assets/js/plugins/bootstrap-notify.js') }}"></script>
+  <script src="./assets/js/plugins/bootstrap-notify.js"></script>
   <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
-  <script src="{{ asset ('assets/js/material-dashboard.js?v=2.1.0') }}"></script>
+  <script src="./assets/js/material-dashboard.js?v=2.1.0"></script>
   <!-- Material Dashboard DEMO methods, don't include it in your project! -->
-  <script src="{{ asset ('assets/demo/demo.js') }}"></script>
+  <script src="./assets/demo/demo.js"></script>
   <script>
     $(document).ready(function() {
       $().ready(function() {
